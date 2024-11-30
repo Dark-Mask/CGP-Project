f2426b9a746ef6596846cd1cf05385171fead1c7
JB_Client <nomail@nomail.com>
2024-11-30 21:45:36 +0800

    ..

--- assets/loader.py
+++ assets/loader.py
@@ -4,4 +4,12 @@ def get_menu():
     return pygame.image.load('assets/images/menu.png')
 
 def menu_background():
-    return pygame.image.load('assets/images/cityskyline.png')
+    return pygame.image.load('assets/images/map/cityskyline.png')
+
+def game_map(map):
+    if map == 'forest':
+        return pygame.image.load('assets/images/map/forest.png')
+    elif map == 'snow':
+        return pygame.image.load('assets/images/map/snow.png')
+    elif map == 'cemetery':
+        return pygame.image.load('assets/images/map/cemetery.png')
--- components/__init__.py
+++ components/__init__.py
@@ -1,10 +1,9 @@
-#import all submodule
-__all__ = ['bullet', 'enemy', 'item', 'map', 'player', 'powerup']
-
 #import submodule
 from . import bullet
 from . import enemy
 from . import item
-from . import map
 from . import player
 from . import powerup
+from . import map_forest
+from . import map_snow
+from . import map_undead
