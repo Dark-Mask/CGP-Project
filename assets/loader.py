017aa1959537c2962b0c632c81ba8cc08316d0d0
JB_Client <nomail@nomail.com>
2024-12-01 12:31:19 +0800

    ..

--- assets/loader.py
+++ assets/loader.py
@@ -4,12 +4,12 @@ def get_menu():
     return pygame.image.load('assets/images/menu.png')
 
 def menu_background():
-    return pygame.image.load('assets/images/map/cityskyline.png')
+    return pygame.image.load('assets/images/background/cityskyline.png')
 
 def game_map(map):
     if map == 'forest':
-        return pygame.image.load('assets/images/map/forest.png')
+        return pygame.image.load('assets/images/background/forest.png')
     elif map == 'snow':
-        return pygame.image.load('assets/images/map/snow.png')
+        return pygame.image.load('assets/images/background/snow.png')
     elif map == 'undead':
-        return pygame.image.load('assets/images/map/undead.png')
+        return pygame.image.load('assets/images/background/undead.png')
