894396af29c49d35dd680a9395ebd5ca2c14c472
JB_Client <nomail@nomail.com>
2024-12-15 19:36:46 +0800

    ..

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ import enemy as en
 from pygame.locals import *
 
 pygame.init()
-
 screen_width = 1200
 screen_height = 700
 tile_size = 50
-
-clock = pygame.time.Clock()
 fps = 60
 
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Jolly Jumpers')
 
 #load resource
-sun_img = pygame.image.load('assets/images/objects/mushroom.png')
 bg_img = pygame.image.load('assets/images/background/cityskyline.png')
 
       
 class World():
-    def __init__(self, data):
+    def __init__(self, data, background):
         self.tile_list = []
+        self.background = background
+        self.enemy_group = pygame.sprite.Group()
 
         #load resources
         dirt = pygame.image.load('assets/images/objects/dirt.png')
@@ -39,7 +38,7 @@ class World():
                     block = grass
                 elif cell == 3:
                     enemy = en.Enemy(col * tile_size, row * tile_size, tile_size, tile_size)
-                    enemy_group.add(enemy)
+                    self.enemy_group.add(enemy)
 
                 if block:
                     img = pygame.transform.scale(block, (tile_size, tile_size))
@@ -56,12 +55,16 @@ class World():
         for line in range(int(screen_width/tile_size)):
             pygame.draw.line(screen, (255,255,255), (line * tile_size, 0), (line * tile_size, screen_height))
 
-
     def draw(self):
+        screen.blit(bg_img, (0,0))
+        self.enemy_group.draw(screen)
         for tile in self.tile_list:
             obj, rect = tile
             screen.blit(obj, rect)
             # pygame.draw.rect(screen, (0, 0, 255), rect, 2)
+
+    def update(self):
+        self.enemy_group.update()
 
 
 #map obstacles
@@ -83,12 +86,7 @@ world_data = [
 ]
 
 
-
-
-
-
-enemy_group = pygame.sprite.Group()
-world = World(world_data)
+world = World(world_data, bg_img)
 player = pl.Player(100, screen_height - 130, 50, 80)
 
 run = True
@@ -99,10 +97,8 @@ while run:
             run = False
 
     #draw assets
-    screen.blit(bg_img, (0,0))
     world.draw()
-    enemy_group.update()
-    enemy_group.draw(screen)
+    world.update()
     player.update(screen, world)
 
     world.draw_grid()
