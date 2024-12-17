250bc4c7ad29280334c0f05697b61d4268855bda
JB_Client <nomail@nomail.com>
2024-12-17 12:31:45 +0800

    ..

--- bullet.py
+++ bullet.py
@@ -2,20 +2,20 @@ import pygame
 
 # Bullet Class
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y, direction_x=0, direction_y=-5):
+    def __init__(self, x, y, velocity_x=0, velocity_y=-5):
         super().__init__()
         self.image = pygame.Surface((5, 10))
         self.image.fill((255,0,0))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
-        self.direction_x = direction_x
-        self.direction_y = direction_y
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
         self.s_width, self.s_height = pygame.display.get_surface().get_size()
 
     def update(self):
-
-        self.rect.x += self.direction_x
-        self.rect.y += self.direction_y
+        self.rect.x += self.velocity_x
+        self.rect.y += self.velocity_y
+            
         # Remove the bullet if it moves off-screen
         if self.rect.bottom < 0 or self.rect.top > self.s_height or self.rect.right < 0 or self.rect.left > self.s_width:
             self.kill()
