231760d5d81bc566ab5cb11bbd8ffcdb44d844e0
JB_Client <nomail@nomail.com>
2024-12-19 07:02:53 +0800

    ..

--- core/winner.py
+++ core/winner.py
@@ -4,62 +4,37 @@ class Winner:
     def __init__(self, game_manager):
         self.width = 800
         self.height = 600
-        self.game_manager = game_manager
 
-        self.img = pygame.image.load('assets/images/background/cityskyline.png')
-        self.img = pygame.transform.scale(self.img, (self.width, self.height))
-        self.menu = pygame.image.load('assets/images/menu.png')
-        self.menu = pygame.transform.scale(self.menu, (450, 500))
-
+        self.background = pygame.image.load('assets/images/background/cityskyline.png')
+        self.background = pygame.transform.scale(self.background, (self.width, self.height))
+        self.summary = pygame.image.load('assets/images/winner.png')
+        self.summary = pygame.transform.scale(self.summary, (550, 500))
 
         self.options = [('Start', (170, 320)), ('Quit', (180, 420))]
         self.font = pygame.font.Font(None, 70)
         self.color = pygame.color.Color(240, 240, 240)
-        self.select_cooldown = 50 #millis
-        self.last_tick = pygame.time.get_ticks()
-        self.selected = 0
 
-        print(f'time: {game_manager.final_time()}')
-        print(f'collected: {game_manager.final_collection()}')
+        self.display_time = self.format_time(game_manager.final_time())
+        self.display_collection = game_manager.final_collection()
 
-    def get_selection(self):
-        if self.selected == 0:
-            return 'start'
-        elif self.selected == 1:
-            return 'quit'
+    
+    def format_time(self, time):
+        minutes = time // 60
+        seconds = time % 60
+
+        return f'{minutes:02}:{seconds:02}'
 
     def draw(self, screen):
-        menu_center_x = (self.width - self.menu.get_width()) // 2
-        menu_center_y = (self.height - self.menu.get_height()) // 2
+        center_x = (self.width - self.summary.get_width()) // 2
+        center_y = (self.height - self.summary.get_height()) // 2
 
         #menu background
-        screen.blit(self.img, (0, 0))
-        screen.blit(self.menu, (menu_center_x, menu_center_y))
+        screen.blit(self.background, (0, 0))
+        screen.blit(self.summary, (center_x, center_y))
 
         #draw options
-        for i in range(len(self.options)):
-            text, coordinate = self.options[i]
-
-            #highligh selection
-            if i == self.selected:
-                color = pygame.color.Color(255, 63, 141)
-            else:
-                color = self.color
-
-            screen.blit(self.font.render(text, True, color), (menu_center_x+coordinate[0], menu_center_y+coordinate[1]))
-
-
-    def update(self):
-        current_tick = pygame.time.get_ticks()
-
-        if current_tick - self.last_tick > self.select_cooldown:
-            self.last_tick = current_tick
-        
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP]:
-                self.selected = 0
-            if keys[pygame.K_DOWN]:
-                self.selected = 1
+        screen.blit(self.font.render(f'{self.display_time}', True, self.color), (center_x, center_y))
+            
 
     def start(self, fps=60):
         screen = pygame.display.set_mode((self.width, self.height))
@@ -72,7 +47,6 @@ class Winner:
                 if event.type == pygame.QUIT:
                     run = False
 
-            self.update()
             self.draw(screen)
 
             keys = pygame.key.get_pressed()
@@ -85,5 +59,4 @@ class Winner:
         
         #close window
         pygame.display.quit()
-        return self.get_selection()
 
